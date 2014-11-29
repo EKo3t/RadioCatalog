111a244a394d13bec1b4912fa253e837c02f8e80
Dmitry Kastsiukevich <dkastsiukevich@scnsoft.com>
2014-11-29 21:35:27 +0300

    Adding themes

---
+++ RadioCatalog/Controllers/ThemeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RadioCatalog.Controllers
+{
+    public class ThemeController : Controller
+    {
+        ThemeModel model = new Models.ThemeModel();
+        const string cookieName = "_theme";
+
+        public ActionResult SetTheme(string theme)
+        {
+            model.CurrentItem = theme;
+
+            HttpCookie cookie = Request.Cookies[cookieName];
+            if (cookie != null)
+                cookie.Value = theme;
+            else
+            {
+                cookie = new HttpCookie(cookieName);
+                cookie.Value = theme;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Theme
+        public ActionResult ChangeTheme()
+        {
+            if (model.CurrentItem == "")
+            {
+                HttpCookie cookie = Request.Cookies[cookieName];
+                if (cookie != null)
+                    model.CurrentItem = cookie.Value;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ RadioCatalog/Models/ThemeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RadioCatalog.Models
+{
+    public class ThemeModel
+    {
+        public ThemeModel()
+        {
+            Items = new List<string>();
+            Items.Add("MSN_Blue");
+            Items.Add("MSN_CherryBlossom");
+        }
+
+        public ThemeModel(string theme)
+        {
+            ThemeModel();
+
+            CurrentItem = theme;
+        }
+
+        public List<string> Items { get; set; }
+        public string CurrentItem { get; set; }
+    }
+}
